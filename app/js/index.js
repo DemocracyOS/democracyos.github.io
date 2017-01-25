c197942257bccca7c265b586c3ac85d4f231c18b
Matías Lescano <mjlescano@protonmail.com>
2017-01-25 14:44:05 -0300

    fix log message

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,1 @@
-console.log('alo!')
+console.log('Free time? Don\'t try to crack this site please, for a better use of your time go to https://github.com/DemocracyOS/democracyos and hack democracy.')
